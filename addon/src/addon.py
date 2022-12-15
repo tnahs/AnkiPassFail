f5248f33e3eb58c3dc1d6124d270fb4867274cc3
Shant Ergenian <shaaant@pm.me>
2022-12-15 00:21:21 -0800

    refactor: update and document whole repo

--- addon/src/addon.py
+++ addon/src/addon.py
@@ -1,5 +1,5 @@
-import os
-from typing import Callable, List, Tuple
+import pathlib
+from typing import Callable
 
 import aqt
 import aqt.gui_hooks
@@ -11,28 +11,65 @@ from .helpers import Defaults, Key
 
 class AnkiPassFail:
     def setup(self) -> None:
-        def hook__disable_shortcuts(
-            state: str, shortcuts: List[Tuple[str, Callable]]
-        ) -> None:
+        """Regsiters the add-on's hooks to append its CSS and disable some reviewer
+        shortcuts."""
 
-            if state != Key.REVIEW:
+        if aqt.mw is None:
+            return
+
+        # Add-ons may expose their own web assets by utilizing
+        # aqt.addons.AddonManager.setWebExports(). Web exports registered in this manner
+        # may then be accessed under the `/_addons` subpath.
+        #
+        # E.g., to allow access to a `my-addon.js` and `my-addon.css` residing in a
+        # "web" subfolder in your add-on package, first register the corresponding web
+        # export:
+        #
+        # > from aqt import mw
+        # > mw.addonManager.setWebExports(__name__, r"web/.*(css|js)")
+
+        aqt.mw.addonManager.setWebExports(
+            __name__, str(pathlib.Path() / Key.SRC / Key.ASSETS / r".*\.css")
+        )
+
+        # The following functions are nested to prevent the need for declaring `self` as
+        # the first argument in order to maintain the correct function signature while
+        # still being able to access `self`.
+
+        def hook__append_css(
+            web_content: aqt.webview.WebContent, context: object
+        ) -> None:
+            """Disables the "Easy" and "Hard" buttons via CSS."""
+
+            if not isinstance(context, aqt.reviewer.ReviewerBottomBar):
                 return
 
-            """
-            class Reviewer:
-                ...
-                def _shortcutKeys():
-                    ...
-                    ("1", lambda: self._answerCard(1)),
-                    ("2", lambda: self._answerCard(2)),
-                    ("3", lambda: self._answerCard(3)),
-                    ("4", lambda: self._answerCard(4)),
-                    ...
+            web_content.css.append(str(Defaults.MAIN_CSS))
+
+        aqt.gui_hooks.webview_will_set_content.append(hook__append_css)
+
+        def hook__disable_shortcuts(
+            state: str, shortcuts: list[tuple[str, Callable]]
+        ) -> None:
+            """Disables the "Easy" and "Hard" hotkeys.
+
+            The shortcuts are defined as a list of tuples with the first entry being the
+            keystore and the second a callable.
+
+            # ...
+            ("1", lambda: self._answerCard(1)),  # Again
+            ("2", lambda: self._answerCard(2)),  # Easy
+            ("3", lambda: self._answerCard(3)),  # Good
+            ("4", lambda: self._answerCard(4)),  # Hard
+            # ...
 
             via https://github.com/ankitects/anki/blob/main/qt/aqt/reviewer.py
             """
 
-            disable: List[int] = []
+            if state != Key.REVIEW:
+                return
+
+            disable: list[int] = []
 
             for index, (shortcut, _) in enumerate(shortcuts):
                 if shortcut in ["2", "4"]:
@@ -42,31 +79,3 @@ class AnkiPassFail:
                 del shortcuts[index]
 
         aqt.gui_hooks.state_shortcuts_will_change.append(hook__disable_shortcuts)
-
-        def hook__append_css(
-            web_content: aqt.webview.WebContent, context: object
-        ) -> None:
-
-            if not isinstance(context, aqt.reviewer.ReviewerBottomBar):
-                return
-
-            # Add-ons may expose their own web assets by utilizing
-            # aqt.addons.AddonManager.setWebExports(). Web exports registered
-            # in this manner may then be accessed under the `/_addons` subpath.
-            #
-            # E.g., to allow access to a `my-addon.js` and `my-addon.css` residing
-            # in a "web" subfolder in your add-on package, first register the
-            # corresponding web export:
-            #
-            # > from aqt import mw
-            # > mw.addonManager.setWebExports(__name__, r"web/.*(css|js)")
-            #
-            # via https://github.com/ankitects/anki/blob/main/qt/aqt/webview.py
-
-            aqt.mw.addonManager.setWebExports(  # type: ignore
-                __name__, fr"{Key.SRC}{os.sep}{Key.ASSETS}{os.sep}.*\.css"
-            )
-
-            web_content.css.append(str(Defaults.MAIN_CSS))
-
-        aqt.gui_hooks.webview_will_set_content.append(hook__append_css)
--- addon/src/helpers.py
+++ addon/src/helpers.py
@@ -3,11 +3,9 @@ import pathlib
 import aqt
 
 
-class ConfigError(Exception):
-    pass
-
-
 class Key:
+    """A class defining re-usable strings."""
+
     ASSETS = "assets"
     MAIN_CSS = "main.css"
     REVIEW = "review"
@@ -15,12 +13,19 @@ class Key:
 
 
 class Defaults:
+    """A class defining all the add-on's default values."""
+
     NAME = "AnkiPassFail"
+
+    # This name is used for properly setting the path to where the web exports
+    # are located. Anki expects this to be: `/_addons/[addon-name]/`. Hard-
+    # coding the name can result in missing web assets as depending on how the
+    # add-on is installed, its name will be different.
     NAME_INTERNAL = (
         aqt.mw.addonManager.addonFromModule(__name__) if aqt.mw is not None else NAME
     )
 
-    # [/absolute/path/to/addon]
+    # [path-to-addon]
     ADDON_ROOT = pathlib.Path(__file__).parent.parent
 
     # /_addons
